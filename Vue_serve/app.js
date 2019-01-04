0e8d41384fca4e407b802542c47b524e3862f91d
linjing-1809WEB <45424654+linjing-1809WEB@users.noreply.github.com>
2019-01-04 18:51:49 +0800

    Add files via upload

--- Vue_serve/app.js
+++ Vue_serve/app.js
@@ -25,13 +25,58 @@ app.use(cors({
 
 //此处开始直接创建路由器  不另外创建路由文件夹。
 
-//功能一：获取用户信息
+
+//功能一：获取商品详情
+//输出json数组{code:1,pagecount:?,data:result} 说明输出成功，页码数，具体数据！ 
+app.get("/getProduct",(req,res)=>{
+	var pno=req.query.pno;
+	var pageSize=req.query.pageSize;
+	if(!pno)pno=1;
+	if(!pageSize)pageSize=18;
+	var reg=/^[0-9]{1,2}$/;
+	if(!reg.test(pno)){
+		res.send({code:-1,msg:"页码格式不正确"});
+		return;
+	}
+	if(!reg.test(pageSize)){
+		res.send({code:-2,msg:"页数格式不正确"});
+		return;
+	}
+	//定义obj.code
+	var progress=0;
+	var obj={code:1};
+	//计算obj.pagecount
+	var fid=req.query.fid;
+	var sql="select count(pid) as cn from fk_product_family where family_pid=?";
+		pool.query(sql,[fid],(err,result)=>{
+		if(err)throw err;
+		obj.pagecount=Math.ceil(result[0].cn/pageSize);
+		progress+=50;
+		if(progress==100){
+			res.send(obj);
+		}
+	})
+	//获取obj.data!
+	//限制显示个数，传开始值offset 和页数 pageSize
+	var offset=parseInt((pno-1)*pageSize);
+	pageSize=parseInt(pageSize);
+	var sql="select * from fk_product_family where family_pid=? limit ?,?";
+	pool.query(sql,[fid,offset,pageSize],(err,result)=>{
+		if(err)throw err;
+		obj.data=result;
+		progress+=50;
+		if(progress==100){
+			res.send(obj);
+		}
+	})
+})
+
 
 
 //功能二：添加用户信息
 
 
-//功能三：获取商品详情
+//功能三：
 
 
 //功能...
